c86c35cc51be2f1c0f5fe6fcd7808b9da2f78e62
shankkalra <shankkalra@gmail.com>
2020-11-22 13:02:10 +0530

    Adding All Service Code

---
+++ user-service/src/main/java/com/grpcflix/user/service/UserService.java
@@ -0,0 +1,47 @@
+package com.grpcflix.user.service;
+
+import com.grpcflix.user.repository.UserRepository;
+import com.personal.grpcflix.common.Genre;
+import com.personal.grpcflix.user.UserGenreUpdateRequest;
+import com.personal.grpcflix.user.UserResponse;
+import com.personal.grpcflix.user.UserSearchRequest;
+import com.personal.grpcflix.user.UserServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.transaction.Transactional;
+
+@GrpcService
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void getUserGenre(UserSearchRequest request, StreamObserver<UserResponse> responseObserver) {
+        UserResponse.Builder builder = UserResponse.newBuilder();
+        this.repository.findById(request.getLoginId())
+                .ifPresent(user -> {
+                    builder.setName(user.getName())
+                            .setLoginId(user.getLogin())
+                            .setGenre(Genre.valueOf(user.getGenre().toUpperCase()));
+                });
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    @Transactional
+    public void updateUserGenre(UserGenreUpdateRequest request, StreamObserver<UserResponse> responseObserver) {
+        UserResponse.Builder builder = UserResponse.newBuilder();
+        this.repository.findById(request.getLoginId())
+                .ifPresent(user -> {
+                    user.setGenre(request.getGenre().toString());
+                    builder.setName(user.getName())
+                            .setLoginId(user.getLogin())
+                            .setGenre(Genre.valueOf(user.getGenre().toUpperCase()));
+                });
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+}
